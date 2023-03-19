c661ea7c18cb81e28f8be6f14014f556e8ca8fd1
aka-azad <ashrafazad86@gmail.com>
2023-03-19 23:26:09 +0600

    lots of update

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,179 @@
 import "./style.scss";
-// const app = document.getElementById("app")! as HTMLDivElement;
+import { create, elIcon, setItem } from "./utils";
+import feather from "feather-icons";
+
+feather.replace({ class: "foo bar", "stroke-width": 1 });
+
+const navBtn = document.querySelector(".nav-logo")! as HTMLDivElement;
+
+navBtn.addEventListener("click", function () {
+  const leftNav = document.querySelector(".nav__left")! as HTMLDivElement;
+
+  if (leftNav.classList.contains("hide")) {
+    leftNav.classList.remove("hide");
+  } else leftNav.classList.add("hide");
+});
+
+//task-input
+interface ITaskList {
+  title: string;
+  desc: string;
+}
+
+const taskList: ITaskList[] = [];
+const taskTitle = document.querySelector(
+  ".input__task-name"
+)! as HTMLInputElement;
+const taskDescription = document.querySelector(
+  ".input__task-description"
+)! as HTMLInputElement;
+
+const submitTask = document.querySelector(".to-do__inputs")! as HTMLFormElement;
+
+const gatherInputValue = function () {
+  const titleValue = taskTitle.value;
+  const descriptionValue = taskDescription.value;
+
+  return { title: titleValue, desc: descriptionValue };
+};
+
+const clearInputs = function () {
+  taskTitle.value = "";
+  taskDescription.value = "";
+};
+const addTask = function (title: string, desc: string): void {
+  const newTask = { title, desc, id: Math.random().toString() };
+
+  taskList.push(newTask);
+  setItem("task-list", taskList);
+  taskComp(title, desc, "1");
+};
+const submitHandler = function (e: Event) {
+  e.preventDefault();
+
+  const userInput = gatherInputValue();
+  const { title, desc } = userInput;
+  addTask(title, desc);
+
+  feather.replace({ class: "foo bar", "stroke-width": 1 });
+  clearInputs();
+};
+
+submitTask.addEventListener("submit", submitHandler);
+
+//list items
+// <li class="to-do__list-cont">
+//                   <div class="to-do__list-cont--top">
+//                     <div class="to-do__left-cont">
+//                       <button class="grab--btn btn-hollow">
+//                         <i data-feather="hexagon"></i>
+//                       </button>
+//                       <div class="radio--btn">
+//                         <input
+//                           type="radio"
+//                           title="task-is-complet"
+//                           id="radio1"
+//                         />
+//                         <label class="task-name" for="radio1">kuch bhi</label>
+//                       </div>
+//                     </div>
+//                     <div class="to-do__right-cont">
+//                       <button class="edit--icon btn-hollow">
+//                         <i data-feather="edit-3"></i>
+//                       </button>
+//                       <button class="more--icon btn-hollow">
+//                         <i data-feather="more-horizontal"></i>
+//                       </button>
+//                     </div>
+//                   </div>
+
+//                   <p class="to-do__dscr">bla bla bla</p>
+//                 </li>
+
+function taskComp(title: string, desc: string, id: string) {
+  const tasks = document.querySelector("#task-list");
+  const list = create({
+    tag: "li",
+    attributes: { className: "to-do__list-cont" },
+    children: [
+      create({
+        tag: "div",
+        attributes: {
+          className: "to-do__list-cont--top",
+        },
+        children: [
+          create({
+            tag: "div",
+            attributes: {
+              className: "to-do__left-cont",
+            },
+            children: [
+              create({
+                tag: "button",
+                attributes: {
+                  className: "grab--btn btn-hollow",
+                },
+                children: [elIcon("hexagon")],
+              }),
+              create({
+                tag: "div",
+                attributes: {
+                  className: "radio--btn",
+                },
+                children: [
+                  create({
+                    tag: "input",
+                    attributes: {
+                      type: "radio",
+                      title: "task-is-complet",
+                      id: "radio1",
+                    },
+                  }),
+                  create({
+                    tag: "label",
+                    attributes: {
+                      className: "task-name",
+                      for: "radio1",
+                      textContent: title,
+                    },
+                  }),
+                ],
+              }),
+            ],
+          }),
+          create({
+            tag: "div",
+            attributes: {
+              className: "to-do__right-cont",
+            },
+            children: [
+              create({
+                tag: "button",
+                attributes: {
+                  className: "edit--icon btn-hollow",
+                },
+                children: [elIcon("edit-3")],
+              }),
+              create({
+                tag: "button",
+                attributes: {
+                  className: "more--icon btn-hollow",
+                },
+                children: [elIcon("more-horizontal")],
+              }),
+            ],
+          }),
+        ],
+      }),
+      create({
+        tag: "p",
+        attributes: {
+          className: "to-do__dscr",
+          textContent: desc,
+        },
+      }),
+    ],
+  });
+  tasks?.appendChild(list);
+  console.log(tasks);
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,56 @@
+export function getItem(key: string): any {
+  return JSON.parse(localStorage.getItem(key)!);
+}
+
+export function setItem(key: string, payload: any): void {
+  localStorage.setItem(key, JSON.stringify(payload));
+}
+// utility function that simplifies creation of new html node
+type ElementType =
+  | "div"
+  | "li"
+  | "img"
+  | "i"
+  | "button"
+  | "input"
+  | "label"
+  | "p";
+
+export function create({
+  tag,
+  appendTo,
+  children = [],
+  attributes = {},
+  events = {},
+}: {
+  tag?: ElementType;
+  appendTo?: Node;
+  children?: Node[];
+  attributes?: { [key: string]: string };
+  events?: { [key: string]: any };
+}) {
+  const element = document.createElement(tag!);
+
+  Object.entries(attributes).forEach(([key, value]) => {
+    //@ts-ignore
+    element[key] = value;
+  });
+
+  Object.entries(events).forEach(([key, value]) => {
+    element.addEventListener(key, value);
+  });
+
+  if (appendTo) {
+    appendTo.appendChild(element);
+  }
+
+  children.forEach((child) => element.appendChild(child));
+
+  return element;
+}
+
+export function elIcon(value: string): Node {
+  const icon = document.createElement("i");
+  icon.setAttribute("data-feather", value);
+  return icon;
+}
